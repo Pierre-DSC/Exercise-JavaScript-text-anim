62b6b038d630e1078da401b88ae681438970e62b
Pierre <descormes.pierre22@gmail.com>
2024-06-15 13:43:47 +0200

    last commit

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 const target = document.getElementById("target");
 let array = ["pierre", "pauline", "cristina"];
+let wordIndex = 0;
+let letterIndex = 0;
 
-target.textContent = array[0];
+const createLetter = () => {
+  const letter = document.createElement ("span");
+  target.appendChild(letter);
 
+  letter.textContent = array[wordIndex][letterIndex];
 
+  setTimeout(() => {
+    letter.remove();
+  }, 2800)
+}
 
-
-console.log(target)
+const loop = () => {
+  setTimeout(() => {
+    if (wordIndex >= array.length) {
+      wordIndex = 0;
+      letterIndex = 0;
+      loop();
+    }else if (letterIndex < array[wordIndex].length) {
+      createLetter();
+    letterIndex++;
+    loop();
+    } else {
+      wordIndex++;
+      letterIndex = 0;
+      setTimeout(() => {
+        loop();
+      }, 2800)
+    }
+  }, 60);
+}
+loop();
